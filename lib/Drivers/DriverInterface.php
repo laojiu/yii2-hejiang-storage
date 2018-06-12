52c74f42beae7e6c0f72e08010ebceb228651d8f
wi1dcard <yokka47@qq.com>
2018-06-12 14:08:44 +0800

    Add phpdoc

--- lib/Drivers/DriverInterface.php
+++ lib/Drivers/DriverInterface.php
@@ -4,5 +4,12 @@ namespace Hejiang\Storage\Drivers;
 
 interface DriverInterface
 {
+    /**
+     * Put a object
+     *
+     * @param string $localFile
+     * @param string $saveTo
+     * @return string|false
+     */
     function put($localFile, $saveTo);
 }
